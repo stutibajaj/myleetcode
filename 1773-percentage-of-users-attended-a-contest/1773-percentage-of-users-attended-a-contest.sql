4a69849ae86cb79b8105b76fc42b9ea56fba9f1f
STUTI BAJAJ <78141532+stutibajaj@users.noreply.github.com>
2023-09-27 00:37:53 +0530

    Time: 1970 ms (58.06%), Space: 0B (100%) - LeetHub

---
+++ 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT contest_id, ROUND(COUNT(DISTINCT user_id)*100/
+(SELECT COUNT(user_id) from Users),2) as percentage
+FROM Register
+GROUP BY contest_id
+ORDER BY percentage DESC, contest_id;
+
+
+
